5a9ba0aa19d59e514f2f6b6f8ba7e83928595264
Dave Sanders <davepaulsanders@gmail.com>
2022-04-06 19:22:31 -0400

    Fixed projects function

--- app.js
+++ app.js
@@ -129,27 +129,25 @@ Add a New Project
       },
     ])
     .then((projectData) => {
+      portfolioData.projects.push(projectData);
       if (projectData.confirmAddProject) {
         return promptProject(portfolioData);
       } else {
         return portfolioData;
       }
-    })
-    .then((projectData) => {
-      portfolioData.projects.push(projectData);
     });
 };
 
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+
+      console.log(
+        "Page created! Check out index.html in this directory to see it!"
+      );
+    });
   });
-
-// const pageHTML = generatePage(userName, github);
-
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
